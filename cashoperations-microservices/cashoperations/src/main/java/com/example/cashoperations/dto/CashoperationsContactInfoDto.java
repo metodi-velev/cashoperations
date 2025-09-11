72e11068aceba757bdaaea9d2706eae002715187
metodi-velev <metodi.velev@yahoo.com>
2025-09-11 20:40:39 +0300

    Add actuator;enable busrefresh API;add spring cloud bus;set up rabbitmq

--- cashoperations-microservices/cashoperations/src/main/java/com/example/cashoperations/dto/CashoperationsContactInfoDto.java
+++ cashoperations-microservices/cashoperations/src/main/java/com/example/cashoperations/dto/CashoperationsContactInfoDto.java
@@ -1,11 +1,17 @@
 package com.example.cashoperations.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cashoperations")
-public record CashoperationsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
-
+@Getter
+@Setter
+public class CashoperationsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
